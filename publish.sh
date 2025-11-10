7d18623b8b034e34abff173e2e4aea551069b422
Ryan Weekley <rweekley@gmail.com>
2025-11-09 20:00:29 -0500

    remove hugo

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 hugo --minify
-rsync -avh -e ssh public/ root@157.245.84.159:/var/www/html/cyberalsolutions.com
+#rsync -avh -e ssh public/ ryan@10.10.0.118:/opt/cyberalsolutions.com/
+rsync -avh -e ssh public/ /home/ryan/repos/CyberalWebsite
+
+
