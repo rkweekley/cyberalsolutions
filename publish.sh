1fd83e1b517e239d346b1f647093de011445d1f6
Ryan Weekley <rweekley@gmail.com>
2026-02-20 16:08:14 -0500

    clean up

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "No build step: site is pure static HTML"
-#rsync -avh -e ssh public/ ryan@10.10.0.118:/opt/cyberalsolutions.com/
-rsync -avh -e ssh public/ /home/ryan/repos/CyberalWebsite
-
-
